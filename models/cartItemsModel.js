4819e0d6554c2ea3600f32adc8645f35aa05afb4
mesh-dell <mwaimonicas@gmail.com>
2024-08-27 20:46:27 +0300

    feat(models): Create cart_items model

---
+++ models/cartItemsModel.js
@@ -0,0 +1,14 @@
+const pool = require("../config/db");
+
+const cartItemsModel = {
+  async createCartItem(cart_id, product_id, quantity) {
+    const query = `INSERT INTO cart_items (cart_id, product_id, quantity)
+       VALUES ($1, $2, $3) RETURNING *;`;
+
+    const values = [cart_id, product_id, quantity];
+    const result = await pool.query(query, values);
+    return result.rows[0];
+  },
+};
+
+module.exports = cartItemsModel;
