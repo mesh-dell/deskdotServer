3364a7aa8e855c27eab84176707eba6b4692b555
mesh-dell <mwaimonicas@gmail.com>
2024-08-28 13:47:58 +0300

    feat(cartModel): Add method to get all products in a buyer's cart

--- models/cartItemsModel.js
+++ models/cartItemsModel.js
@@ -27,6 +27,23 @@ const CartItemsModel = {
     const result = await pool.query(query, values);
     return result.rows[0];
   },
+
+  async getAllProducts(cart_id) {
+    const query = "SELECT * FROM cart_items WHERE cart_id =$1;";
+    const values = [cart_id];
+
+    const result = await pool.query(query, values);
+    return result.rows;
+  },
+
+  async getByProductId(product_id, cart_id) {
+    const query =
+      "SELECT * FROM cart_items WHERE product_id = $1 AND cart_id = $2;";
+    const values = [product_id, cart_id];
+
+    const result = await pool.query(query, values);
+    return result.rows;
+  },
 };
 
 module.exports = CartItemsModel;
--- models/cartModel.js
+++ models/cartModel.js
@@ -9,6 +9,13 @@ const CartModel = {
     const result = await pool.query(query, values);
     return result.rows[0];
   },
+
+  async findByBuyerId(buyer_id) {
+    const query = "SELECT * FROM carts WHERE buyer_id = $1";
+    const values = [buyer_id];
+    const result = await pool.query(query, values);
+    return result.rows;
+  },
 };
 
 module.exports = CartModel;
