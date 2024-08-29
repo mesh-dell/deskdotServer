324ac1dc6fa6800cd580dbaf46aa1bc2bbfbc362
mesh-dell <mwaimonicas@gmail.com>
2024-08-29 23:07:04 +0300

    feat(controller): Make order controller

---
+++ controllers/orderController.js
@@ -0,0 +1,36 @@
+const OrderModel = require("../models/orderModel");
+
+const OrderController = {
+  async createOrder(req, res) {
+    try {
+      const order = await OrderModel.createOrder(req.params.buyer_id);
+      res.status(201).json(order);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+
+  async cancelOrder(req, res) {
+    try {
+      const order = await OrderModel.cancelOrder(req.params.order_id);
+      if (order) {
+        res.json(order);
+      } else {
+        res.status(404).json({ message: "Order not found" });
+      }
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+
+  async findByBuyerId(req, res) {
+    try {
+      const orders = await OrderModel.findByBuyerId(req.params.buyer_id);
+      res.json(orders);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+};
+
+module.exports = OrderController;
