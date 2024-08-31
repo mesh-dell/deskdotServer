3263e8e2a35494c4c052978da493f99c707455ea
mesh-dell <mwaimonicas@gmail.com>
2024-08-31 21:54:09 +0300

    refactor(cartItemModel): Change schema to have unique product in a cart
    Now errors can be detected in the schema rather than checking if a
    product alredy exists in cart before adding it

--- models/cartItemsModel.js
+++ models/cartItemsModel.js
@@ -30,16 +30,6 @@ const CartItemsModel = {
   async findByCartId(cart_id) {
     const query = "SELECT * FROM cart_items WHERE cart_id =$1;";
     const values = [cart_id];
-
-    const result = await pool.query(query, values);
-    return result.rows;
-  },
-
-  async getByProductId(product_id, cart_id) {
-    const query =
-      "SELECT * FROM cart_items WHERE product_id = $1 AND cart_id = $2;";
-    const values = [product_id, cart_id];
-
     const result = await pool.query(query, values);
     return result.rows;
   },
