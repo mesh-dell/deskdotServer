b6047333f45fb923bf5a18da51cdc7680e0ac29b
mesh-dell <mwaimonicas@gmail.com>
2024-08-31 21:00:33 +0300

    refactor(models): Update remove item method to use cart_item_id to delete

--- models/cartItemsModel.js
+++ models/cartItemsModel.js
@@ -10,11 +10,10 @@ const CartItemsModel = {
     return result.rows[0];
   },
 
-  async removeItem(cart_id, product_id) {
-    const query =
-      "DELETE FROM cart_items WHERE cart_id = $1 AND product_id = $2 RETURNING *;";
+  async removeItem(cart_item_id) {
+    const query = "DELETE FROM cart_items WHERE cart_item_id = $1 RETURNING *;";
 
-    const values = [cart_id, product_id];
+    const values = [cart_item_id];
     const result = await pool.query(query, values);
     return result.rows[0];
   },
