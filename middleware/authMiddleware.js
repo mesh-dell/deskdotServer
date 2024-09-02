874401e5a028aefc3ea26c7347fe09305247d4ca
mesh-dell <mwaimonicas@gmail.com>
2024-09-02 20:52:07 +0300

    feat(auth): Add auth middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const authMiddleware = (req, res, next) => {
+  // Get token from header
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  // Check if no token
+  if (!token) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+
+    // Add user from payload to result
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
+
+module.exports = authMiddleware;
