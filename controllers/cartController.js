bd4687c3da8ea52d9d650f06208d4a41d21aea69
mesh-dell <mwaimonicas@gmail.com>
2024-09-05 00:08:36 +0300

    refactor(cartController): Update cart methods to use user object

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,27 +1,96 @@
 const CartModel = require("../models/cartModel");
+const CartItemsModel = require("../models/cartItemsModel");
 
 const CartController = {
-  async create(req, res) {
+  async getCart(req, res, next) {
     try {
-      const { buyer_id } = req.body;
-      const cart = await CartModel.createCart(buyer_id);
-      res.status(201).json(cart);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
+      const { id } = req.user;
+      const cart = await CartModel.findByBuyerId(id);
+
+      if (!cart) {
+        return res.status(404).json({ message: "Cart not found" });
+      }
+
+      const { cart_id } = cart;
+      const cartItems = await CartItemsModel.findByCartId(cart_id);
+
+      res.json(cartItems);
+    } catch (error) {
+      next(error);
     }
   },
 
-  async findByBuyerId(req, res) {
+  async addToCart(req, res, next) {
     try {
-      const cart = await CartModel.findByBuyerId(req.params.buyer_id);
+      const { id } = req.user;
+      const cart = await CartModel.findByBuyerId(id);
 
-      if (cart) {
-        res.json(cart);
-      } else {
-        res.status(404).json({ message: "Cart not found" });
+      if (!cart) {
+        cart = await CartModel.createCart(id);
       }
-    } catch (err) {
-      res.status(500).json({ message: err.message });
+
+      const { product_id, quantity } = req.body;
+      const cartItem = await CartItemsModel.addItem(
+        cart.cart_id,
+        product_id,
+        quantity
+      );
+      res.status(201).json(cartItem);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async updateQuantity(req, res, next) {
+    try {
+      const id = req.user.id;
+      const { quantity } = req.body;
+      const { cart_item_id } = req.params;
+
+      const cart = await CartModel.findByBuyerId(id);
+
+      if (!cart) {
+        return res.status(404).json({ message: "Cart not found" });
+      }
+
+      const cartItem = await CartItemsModel.getCartItem(cart_item_id);
+
+      if (!cartItem) {
+        return res.status(404).json({ message: "Item not found in the cart" });
+      }
+
+      const updatedCartItem = await CartItemsModel.updateQuantity(
+        cart_item_id,
+        quantity
+      );
+
+      res.json(updatedCartItem);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async remove(req, res, next) {
+    try {
+      const id = req.user.id;
+      const { cart_item_id } = req.params;
+
+      const cart = await CartModel.findByBuyerId(id);
+
+      if (!cart) {
+        return res.status(404).json({ message: "Cart not found" });
+      }
+
+      const cartItem = await CartItemsModel.getCartItem(cart_item_id);
+
+      if (!cartItem) {
+        return res.status(404).json({ message: "Item not found in the cart" });
+      }
+
+      const removedItem = await CartItemsModel.removeItem(cart_item_id);
+      res.json({ message: "Item removed from cart", removedItem });
+    } catch (error) {
+      next(error);
     }
   },
 };
