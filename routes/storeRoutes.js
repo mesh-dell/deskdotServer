409e873bc0e96b74e6440e7ae3b770015214bc1b
mesh-dell <mwaimonicas@gmail.com>
2024-09-06 00:35:20 +0300

    feat(store): Add route to get orders to a store

--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const StoreController = require("../controllers/storeController");
+const OrderController = require("../controllers/orderController");
 
 router.get("/", StoreController.getAllStores);
-
 router.get("/:id", StoreController.getStore);
-
 router.get("/:id/products", StoreController.getStoreProducts);
+router.get("/orders", OrderController.getSellerOrders);
 
 module.exports = router;
