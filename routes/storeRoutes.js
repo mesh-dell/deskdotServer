c4b43b2076c4f10255c0209985a210f3a018c6c6
mesh-dell <mwaimonicas@gmail.com>
2024-09-07 18:40:29 +0300

    fix(storeRoutes): Make 'getsellerorders' a protected route

--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const StoreController = require("../controllers/storeController");
 const OrderController = require("../controllers/orderController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/", StoreController.getAllStores);
 router.get("/:id", StoreController.getStore);
 router.get("/:id/products", StoreController.getStoreProducts);
-router.get("/orders", OrderController.getSellerOrders);
+router.get("/orders", authMiddleware, OrderController.getSellerOrders);
 
 module.exports = router;
